96b37cc16d0aeb76d397e63aaf23daa8ab6689c4
samipjain <samipjain1216@gmail.com>
2017-10-14 19:48:08 +0530

    About Us assignment + Angular Template-driven forms

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
 @Component({
   selector: 'app-aboutus',
   templateUrl: './aboutus.component.html',
   styleUrls: ['./aboutus.component.scss']
 })
 export class AboutusComponent implements OnInit {
+  leaders: Leader[];
 
-  constructor() { }
+  constructor(private leaderService: LeaderService) { }
 
   ngOnInit() {
+    this.leaders = this.leaderService.getLeaders();
   }
 
 }
