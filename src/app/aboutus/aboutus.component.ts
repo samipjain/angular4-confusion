3cf02122c589da93b1d732d2101be6bd2d9ca371
samipjain <samipjain1216@gmail.com>
2017-10-22 19:28:31 +0530

    Promise part 1

--- src/app/aboutus/aboutus.component.ts
+++ src/app/aboutus/aboutus.component.ts
@@ -14,7 +14,8 @@ export class AboutusComponent implements OnInit {
   constructor(private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.leaders = this.leaderService.getLeaders();
+    this.leaderService.getLeaders()
+    .then(leaders => this.leaders = leaders);
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,9 +22,11 @@ export class HomeComponent implements OnInit {
     private leaderService: LeaderService) { }
 
   ngOnInit() {
-    this.dish = this.dishService.getFeaturedDish();
+    this.dishService.getFeaturedDish()
+      .then(dish => this.dish = dish);
     this.promotion = this.promotionService.getFeaturedPromotion();
-    this.leader = this.leaderService.getFeaturedLeader();
+    this.leaderService.getFeaturedLeader()
+      .then(leader => this.leader = leader);
   }
 
 }
