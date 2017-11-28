fdf085cad1620e46d1ac895b56be5e26b7863d37
samipjain <samipjain1216@gmail.com>
2017-11-28 23:53:01 +0530

    Custom directives

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,9 +3,11 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
+import { Comment } from '../shared/comment';
 
 import 'rxjs/add/operator/switchMap';
 
@@ -21,14 +23,35 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
+  comment: Comment;
   errMess: string;
 
-  constructor(private dishservice: DishService,
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required': 'Author name is required',
+      'minlength': 'Author name must be at least 2 characters'
+    },
+    'comment': {
+      'required': 'Comment is required'
+    }
+  };
+
+  commentForm: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private dishservice: DishService,
     private route: ActivatedRoute,
     private location: Location,
     @Inject('BaseURL') private BaseURL) { }
 
   ngOnInit() {
+    this.createForm();
+
     this.dishservice.getDishIds()
       .subscribe(dishIds => this.dishIds = dishIds);
 
@@ -54,4 +77,35 @@ export class DishdetailComponent implements OnInit {
     this.location.back();
   }
 
+  createForm(): void {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      rating: 5,
+      comment: ['', Validators.required]
+    });
+
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+  }
+
+  onsubmit(): void {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    console.log(this.comment);
+    // this.dish.comments.push(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this.dishcopy.save()
+      .subscribe(dish => this.dish = dish);
+    this.commentForm.reset({
+      author: '',
+      rating: 5,
+      comment: ''
+    });
+  }
+
+  onValueChanged(data?: any): void {
+
+  }
 }
